edd2df459c3378f206a16dd8cadcbc670a23edd1
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2024-12-18 20:34:23 -0300

    add second cam ?

--- Component/loged_user_ui.py
+++ Component/loged_user_ui.py
@@ -4,17 +4,19 @@ import cv2
 import threading
 from tkinter import Label, Frame
 from PIL import Image, ImageTk
-
 def show_camara():
     global on
     global cam_lbl
+    global cam_lbl1
     global start_btn
+    global cam2
     if on is not None:
         ret , frame = on.read()
         if ret == True:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            if frame is not None and frame.size > 0:
-                im = Image.fromarray(frame)
+            frame_resized = cv2.resize(frame,(250,250))
+            if frame_resized is not None and frame_resized.size > 0:
+                im = Image.fromarray(frame_resized)
                 img = ImageTk.PhotoImage(im)
                 cam_lbl.configure(image = img)
                 cam_lbl.image = img
@@ -26,16 +28,35 @@ def show_camara():
         else:
             cam_lbl.configure(text="DETENIDO")
             start_btn.configure(text="Comenzar")
+            cam_lbl1.configure(text="DETENIDO")
+            #cerrar las camaras en grl
             on.release()
+            cam2.release()
+    if cam2 is not None:
+        ret, frame2 = cam2.read()
+        if ret == True:
+            frame2 = cv2.cvtColor(frame2,cv2.COLOR_BGR2RGB)
+            frame_resized2 = cv2.resize(frame2,(250,250))
+            if frame_resized2 is not None and frame_resized2.size > 0:
+                im2 = Image.fromarray(frame_resized2)
+                img2 = ImageTk.PhotoImage(im2)
+                cam_lbl1.configure(image= img2)
+                cam_lbl1.image = img2
+                cam_lbl1.after(10,show_camara)
+                cam_lbl1.configure(text="")
+
 def start_capture():
     global on
+    global cam2
     #global cam_lbl
     on = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    cam2 = cv2.VideoCapture(1,cv2.CAP_DSHOW)
     show_camara()
 
 def loged_user(usuario,root):
     global cam_lbl
     global start_btn
+    global cam_lbl1
     root.destroy()
     ctk.set_appearance_mode("dark")
     root = ctk.CTk()
@@ -59,8 +80,11 @@ def loged_user(usuario,root):
 
     cam_lbl = ctk.CTkLabel(root,text="")
     cam_lbl.place(x=20,y=200)
+    cam_lbl1 = ctk.CTkLabel(root,text="")
+    cam_lbl1.place(x=400,y=200)
 
 
 
     root.mainloop()
 on = None
+cam2 = None
