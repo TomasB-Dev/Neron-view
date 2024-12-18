2d2d8608a52a303f144f5b1c1daae5d8f522a44a
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2024-12-18 14:50:15 -0300

    show 1 camera

--- Component/loged_user_ui.py
+++ Component/loged_user_ui.py
@@ -5,8 +5,37 @@ import threading
 from tkinter import Label, Frame
 from PIL import Image, ImageTk
 
+def show_camara():
+    global on
+    global cam_lbl
+    global start_btn
+    if on is not None:
+        ret , frame = on.read()
+        if ret == True:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if frame is not None and frame.size > 0:
+                im = Image.fromarray(frame)
+                img = ImageTk.PhotoImage(im)
+                cam_lbl.configure(image = img)
+                cam_lbl.image = img
+                cam_lbl.after(10,show_camara)
+                start_btn.configure(text="Detener")
+                cam_lbl.configure(text="")
+            else:
+                print("tamaño incorrecto")
+        else:
+            cam_lbl.configure(text="DETENIDO")
+            start_btn.configure(text="Comenzar")
+            on.release()
+def start_capture():
+    global on
+    #global cam_lbl
+    on = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    show_camara()
 
 def loged_user(usuario,root):
+    global cam_lbl
+    global start_btn
     root.destroy()
     ctk.set_appearance_mode("dark")
     root = ctk.CTk()
@@ -23,19 +52,15 @@ def loged_user(usuario,root):
 
     start_btn = ctk.CTkButton(
         root,text="Comenzar",font=LOGIN_FONT,
-        height=HEIGHT_BOTONES,corner_radius=10,border_width=2
+        height=HEIGHT_BOTONES,corner_radius=10,border_width=2,
+        command=start_capture
             )
     start_btn.place(x=200,y=20)
 
-    stop_btn = ctk.CTkButton(
-        root,text="Detener",font=LOGIN_FONT,
-        height=HEIGHT_BOTONES,corner_radius=10,border_width=2
-        )
-    stop_btn.place(x=380,y=20)
-
-
+    cam_lbl = ctk.CTkLabel(root,text="")
+    cam_lbl.place(x=20,y=200)
 
 
 
     root.mainloop()
-    
+on = None
