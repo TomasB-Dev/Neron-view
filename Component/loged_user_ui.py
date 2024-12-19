856785e93130ac7840c64c9adb3afe22f2d31522
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2024-12-19 11:34:17 -0300

    fix cameras

--- Component/loged_user_ui.py
+++ Component/loged_user_ui.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="Component/.env")
 cam1 = None
 cam2 = None
+cam3 = None
 stop_threads = False
 def show_camera(cam, cam_lbl):
     global stop_threads
@@ -28,40 +29,62 @@ def show_camera(cam, cam_lbl):
         cv2.waitKey(10)
 
 def start_capture():
-    global cam1, cam2, stop_threads, cam_lbl, cam_lbl1, start_btn
-    cam1 = os.getenv("CAM1")
-    if cam1 is None and cam2 is None:
-        
+    global cam1, cam2, cam3, stop_threads, cam_lbl1, cam_lbl2, cam_lbl3, start_btn
+
+    camara1 = os.getenv("CAM1")
+    camara2 = os.getenv("CAM2")
+
+
+    if cam1 is None and cam2 is None and cam3 is None:
+        # Inicializar cámaras
         cam1 = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        cam2 = cv2.VideoCapture(cam1)
+        cam2 = cv2.VideoCapture(camara1)
+        cam3 = cv2.VideoCapture(camara2)
 
+        # Verificar cada cámara
         if not cam1.isOpened():
-            cam_lbl.configure(text="Error al abrir la camara")
-            return
+            cam_lbl1.configure(text="Error al abrir la camara 1")
+            cam1 = None
+        else:
+            threading.Thread(target=show_camera, args=(cam1, cam_lbl1), daemon=True).start()
+
         if not cam2.isOpened():
-            cam_lbl1.configure(text="Error al abrir la camara")
-            return
+            cam_lbl2.configure(text="Error al abrir la camara 2")
+            cam2 = None
+        else:
+            threading.Thread(target=show_camera, args=(cam2, cam_lbl2), daemon=True).start()
 
+        if not cam3.isOpened():
+            cam_lbl3.configure(text="Error al abrir la camara 3")
+            cam3 = None
+        else:
+            threading.Thread(target=show_camera, args=(cam3, cam_lbl3), daemon=True).start()
 
-        stop_threads = False
-        threading.Thread(target=show_camera, args=(cam1, cam_lbl), daemon=True).start()
-        threading.Thread(target=show_camera, args=(cam2, cam_lbl1), daemon=True).start()
-
+        # Actualizar texto del botón
         start_btn.configure(text="Detener")
+
     else:
-        
         stop_threads = True
-        cam1.release()
-        cam2.release()
-        cam_lbl.configure(image=None, text="DETENIDO")
-        cam_lbl1.configure(image=None, text="DETENIDO")
+
+        if cam1:
+            cam1.release()
+            cam_lbl1.configure(image=None, text="DETENIDO")
+        if cam2:
+            cam2.release()
+            cam_lbl2.configure(image=None, text="DETENIDO")
+        if cam3:
+            cam3.release()
+            cam_lbl3.configure(image=None, text="DETENIDO")
+
+        # Resetear variables
         start_btn.configure(text="Comenzar")
         cam1 = None
         cam2 = None
+        cam3 = None
 
 def loged_user(usuario, root):
 
-    global cam_lbl, start_btn, cam_lbl1
+    global cam_lbl1, start_btn, cam_lbl2, cam_lbl3
     root.destroy()
     ctk.set_appearance_mode("dark")
     root = ctk.CTk()
@@ -81,10 +104,16 @@ def loged_user(usuario, root):
         command=start_capture
     )
     start_btn.place(x=200, y=20)
-
-    cam_lbl = ctk.CTkLabel(root, text="")
-    cam_lbl.place(x=20, y=200)
+    #Espacios para las camaras
     cam_lbl1 = ctk.CTkLabel(root, text="")
-    cam_lbl1.place(x=400, y=200)
+    cam_lbl1.place(x=20, y=100)
+
+    cam_lbl2 = ctk.CTkLabel(root, text="")
+    cam_lbl2.place(x=400, y=100)
+
+    cam_lbl3 = ctk.CTkLabel(root,text="")
+    cam_lbl3.place(x=780,y=100)
+
+
 
     root.mainloop()
